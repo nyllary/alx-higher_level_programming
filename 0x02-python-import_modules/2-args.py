3d455fe37d201d31768a25d3890dd8131aff3fd9
Benjamin Laryea <nyllary@gmail.com>
2024-01-06 13:27:56 +0000

    Task 2, printing arguments in numbers

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-import sys
-argc = len(sys.argv)
+if __name__ == "__main__":
+    from sys import argv
+
+argc = len(argv)
 
 if argc == 1:
     print(argc - 1, "arguments.")
 elif argc == 2:
     print(argc - 1, "argument:")
-    print("1:", sys.argv[1])
+    print("1:", argv[1])
 else:
     print(argc - 1, "arguments:")
     for i in range(1, argc):
         print(i, end=": ")
-        print(sys.argv[i])
+        print(argv[i])
